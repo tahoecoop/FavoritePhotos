baeb883e18c27b8158b3deaa0ec72de6c3afe133
tahoecoop <username@users.noreply.github.com>
2015-07-30 14:37:45 -0700

    Initial Commit

---
+++ FavoritePhotos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FavoritePhotos
+//
+//  Created by Benjamin COOPER on 7/30/15.
+//  Copyright (c) 2015 Ben Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
